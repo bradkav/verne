6e76596c482ca89bbdc0d7ac47a155ca2c3555ab
Bradley Kavanagh <kavanaghb@login1.ifca.es>
2022-10-13 13:21:33 +0200

    Added reflection

--- src/CalcVelDist_function.py
+++ src/CalcVelDist_function.py
@@ -12,12 +12,19 @@ except:
     def tqdm(x):
         return x
 
+d_list, p_list = np.loadtxt("data/p_reflected.txt", unpack=True)
+p_interp = interp1d(d_list, p_list, bounds_error=False, fill_value=(0, 1))
+
+
+
+
 def calcVelDist_full(m_x, sigma_p, loc, interaction, depth_in = 0):
     
+
     results_dir = "results/"
 
 
-    if (interaction not in  ["SI", "SD"]):
+    if (interaction not in  ["SI", "SD", "Millicharge", "hDP"]):
         print(">Unknown interaction type <", interaction, ">...")
         exit()
 
@@ -37,6 +44,32 @@ def calcVelDist_full(m_x, sigma_p, loc, interaction, depth_in = 0):
         depth = depth_in
     
     target = loc
+
+    def p_return_full(theta, sigma):
+        mp = 0.92
+        mA = 16*mp
+        coherent = (mA/mp)**4*(m_x + mp)**2/(m_x + mA)**2
+
+        n_E = 3e22 #cm^-3
+    
+        lam = 1/(coherent*sigma_p*n_E)
+        d = 1e2*verne.pathLength_Earth(depth, np.pi - theta)
+        return p_interp(d/lam)
+
+    def calc_frac(sigma, gamma): 
+
+        Nv = 100
+    
+        vs = np.linspace(1e-3, 800, Nv)
+        thetas = np.linspace(0, np.pi, 25)
+        vals = np.zeros(Nv)
+
+        for i in tqdm(range(Nv)):
+            integs = p_return_full(thetas, sigma)*vs[i]**2*np.sin(thetas)*np.vectorize(MB.calcf_integ)(vs[i], thetas, gamma=gamma)
+            vals[i] = np.trapz(integs, thetas)
+
+        frac = np.trapz(vals, vs)
+        return frac    
 
 
     print(" ")
@@ -72,8 +105,9 @@ def calcVelDist_full(m_x, sigma_p, loc, interaction, depth_in = 0):
     
         #Calculate the maximum final speed as a function of incoming angle theta
         v_final_max = 0.0*v_initial_max
+        margin = 1.05
         for i in tqdm(range(Nvals)):
-            v_final_max[i] = verne.calcVfinal_full(v_initial_max[i], thetavals[i],  depth, sigma_p, m_x, interaction, target)
+            v_final_max[i] = margin*verne.calcVfinal_full(v_initial_max[i], thetavals[i],  depth, sigma_p, m_x, interaction, target)
     
         #Calculate interpolation function for max final speed
         vmax = np.max(v_final_max)
@@ -87,9 +121,10 @@ def calcVelDist_full(m_x, sigma_p, loc, interaction, depth_in = 0):
         #Tabulate values of speed distribution
     
         #Generate a list of sampling values for v (with some very close to v_th)
-        vlist = np.logspace(np.log10(v_th), np.log10(0.25*vmax), Nv1)    #20
-        vlist = np.append(vlist, np.linspace(0.15*vmax, 0.999*vmax, Nv2)) #40
-        vlist = np.append(vlist, 19.9)
+        vlist = np.geomspace(v_th, 0.25*vmax, Nv1)    #20
+        vlist = np.append(vlist, np.linspace(0.15*vmax, 0.6*vmax, Nv2)) #40
+        vlist = np.append(vlist, np.linspace(0.61*vmax, 0.9999*vmax, Nv3)) 
+        vlist = np.append(vlist, 0.99*v_th)
         #vlist = np.linspace(v_th, 0.999*vmax, 50)
         vlist = np.sort(vlist)
         f_final = 0.0*vlist
@@ -100,32 +135,39 @@ def calcVelDist_full(m_x, sigma_p, loc, interaction, depth_in = 0):
         vlist = np.append(vlist, vmax)
         f_final = np.append(f_final, 0.0)
     
-        return vlist, f_final
+        frac_reflected = calc_frac(sigma_p, gamma)
+
+        return vlist, f_final, f_final*(1 + frac_reflected)
         
         
     #Loop over gamma values
     N_gamma = 25
     Nv1 = 20 #Near the velocity threshold
-    Nv2 = 40 #Everywhere else
-    Nv = Nv1 + Nv2 + 1 + 1 #Add an extra one for 20 km/s
-    gamma_list = np.linspace(0, 1.0, N_gamma)
+    Nv2 = 30 #Everywhere else
+    Nv3 = 48
+    Nv = Nv1 + Nv2 + Nv3 + 1 + 1 #Add an extra one for 20 km/s
+    gamma_list = np.linspace(0, 1, N_gamma)
+    gamma_list[0] = 1e-3
+    gamma_list[-1] = 1 - 1e-3
     vgrid = np.zeros((N_gamma, Nv))
     fgrid = np.zeros((N_gamma, Nv))
+    fgrid_withrefl = np.zeros((N_gamma, Nv))
     for j in range(N_gamma):
         print(">Calculating for gamma/pi = ", gamma_list[j],"...")
-        vgrid[j,:], fgrid[j,:] = getVelDist(gamma_list[j]*np.pi)
+        vgrid[j,:], fgrid[j,:], fgrid_withrefl[j,:] = getVelDist(gamma_list[j]*np.pi)
     
     gamma_rep = np.repeat(gamma_list, Nv)
     
     #Output to file
     fname = results_dir + "veldists/f_" + interaction + "_" + loc + "_mx" + '{0:.3f}'.format(m_x) + "_lsig" + '{0:.2f}'.format(np.log10(sigma_p)) + ".txt"
     headertxt = "mx [GeV]: " + str(m_x) + "\nsigma_p [cm^2]: " + str(sigma_p) + "\ndepth [m]: " + str(depth) + "\nloc: " + target
-    headertxt += "\nColumns: gamma/pi, v [km/s], f(v, gamma) [s/km]"
+    headertxt += "\nColumns: gamma/pi, v [km/s], f(v, gamma) [s/km], f[with reflection](v, gamma) [s/km]"
     
     #Try different locations to save files, depending on where
     #the code is being executed from
+    outdata = np.transpose([gamma_rep, vgrid.flatten(), fgrid.flatten(), fgrid_withrefl.flatten()])
     try:
-        np.savetxt(fname, np.transpose([gamma_rep, vgrid.flatten(), fgrid.flatten()]), header=headertxt)
+        np.savetxt(fname, outdata, header=headertxt)
     except:
-        np.savetxt("../" + fname, np.transpose([gamma_rep, vgrid.flatten(), fgrid.flatten()]), header=headertxt)
+        np.savetxt("../" + fname, outdata, header=headertxt)
     
